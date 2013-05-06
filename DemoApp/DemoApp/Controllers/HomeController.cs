bdb04f6649642d04adfffa5eee076c2505b66625
Serg Melikyan <smelikyan@mirantis.com>
2013-05-06 20:06:58 +0400

    Initial version of Murano MVC Demo

---
+++ DemoApp/DemoApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.DirectoryServices;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Web.Mvc;
+using DemoApp.Models;
+
+namespace DemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new Model
+            {
+                MachineName = Environment.MachineName,
+                IPs = GetIPs(),
+                AppPool = Request.ServerVariables["APP_POOL_ID"] ?? "Unknown"
+            };
+
+            return View(model);
+        }
+
+        private string GetIPs()
+        {
+            var hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            return string.Join(", ", hostEntry.AddressList
+                               .Where(x => x.AddressFamily == AddressFamily.InterNetwork)
+                               .Select(x => x.ToString())
+            );
+        }
+    }
+}
---
+++ DemoApp/DemoApp/Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace DemoApp.Models
+{
+    public class Model
+    {
+        [DisplayName("IP")]
+        public string IPs { get; set; }
+        [DisplayName("Machine Name")]
+        public string MachineName { get; set; }
+        [DisplayName("Application Pool")]
+        public string AppPool { get; set; }
+    }
+}
